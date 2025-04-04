8bd1cdda2537256bf2ca465b3b2a599b961f4569
Kyle Loveless <kjloveless321@gmail.com>
2025-04-04 12:05:29 -0500

    begin ui

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+  "html/template"
 	"net/http"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.Write([]byte("visit <a href='/login'>/login</a> to get started"))
+  files := []string{
+    "./ui/html/base.tmpl",
+    "./ui/html/pages/home.tmpl",
+  }
+
+  ts, err := template.ParseFiles(files...)
+  if err != nil {
+    app.logger.Error(err.Error())
+    http.Error(w, "internal server error", http.StatusInternalServerError)
+    return
+  }
+
+  err = ts.ExecuteTemplate(w, "base", nil)
+  if err != nil {
+    app.logger.Error(err.Error())
+    http.Error(w, "internal server error", http.StatusInternalServerError)
+  }
 }
